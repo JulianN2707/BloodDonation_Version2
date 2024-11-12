cf55a04a161dda96aad30e72821b5482ae0d33f5
Julian Andres Niño Niño <desarrolladorP1@PASSWORDCONSULTING.onmicrosoft.com>
2024-11-11 21:51:17 -0500

    app

--- DonacionSangre/Domain /Common/AppSettings.cs
+++ DonacionSangre/Domain /Common/AppSettings.cs
@@ -4,5 +4,8 @@ namespace DonacionSangre.Domain.Common;
 
 public class AppSettings
 {
+    public const string SectionKey = "ConnectionStrings";
+
+    public string ConnectionString { get; set; } = string.Empty;
 
 }
