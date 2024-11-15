11d2c9d520f1f67b2a5259e7d4834a991bfb8ffe
Nicolas Muñoz <bbohorquez@unicauca.edu.co>
2024-11-14 23:13:24 -0500

    feat: Implementacion componente front para crear donante

--- BloodDonationUI/blood-donation/src/app/app.routes.ts
+++ BloodDonationUI/blood-donation/src/app/app.routes.ts
@@ -2,9 +2,11 @@
 import { Routes } from '@angular/router';
 import { DonationsComponent } from './components/donations/donations.component';
 import { LoginComponent } from './components/login/login.component';
+import { CreateUsuarioComponent } from './components/create-usuario/create-usuario.component';
 
 export const routes: Routes = [
   { path: '', component: DonationsComponent }, // Ruta por defecto
   { path: 'login', component: LoginComponent },
+  { path: 'create-usuario', component: CreateUsuarioComponent },
   // Otras rutas
 ];
