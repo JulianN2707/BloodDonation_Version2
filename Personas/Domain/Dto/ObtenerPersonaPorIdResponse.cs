7142d1c323c16becb7c264d57dffe0522db9ed77
Nicolas Muñoz <bbohorquez@unicauca.edu.co>
2024-11-14 23:17:08 -0500

    feat: Creacion servicio para obtener persona por id

---
+++ Personas/Domain/Dto/ObtenerPersonaPorIdResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Personas.Domain.Dto
+{
+    public class ObtenerPersonaPorIdResponse
+    {
+        public Guid PersonaId { get; set; }
+        public string NumeroDocumento { get; set; } = null!;
+        public DateTime? FechaExpedicionDocumento { get; set; }
+        public string? PrimerApellido { get; set; }
+        public string? PrimerNombre { get; set; }
+        public string? SegundoApellido { get; set; }
+        public string? SegundoNombre { get; set; }
+        public Guid? MunicipioDireccionId { get; set; }
+        public string? CorreoElectronico { get; set; }
+        public string? Celular { get; set; }
+        public string? Direccion { get; set; }
+    }
+}
